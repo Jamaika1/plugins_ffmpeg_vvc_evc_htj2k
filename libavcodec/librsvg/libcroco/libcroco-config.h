e7d7153f420d04027c5d142b76d165ee602ec99b
Jamaika1 <lukaszcz18@wp.pl>
2023-03-07 21:56:08 +0100

    Added croco inkscape/libcroco@b9e4b47 (#24)

---
+++ libavcodec/librsvg/libcroco/libcroco-config.h
@@ -0,0 +1,17 @@
+#ifndef LIBCROCO_VERSION_NUMBER
+//define LIBCROCO_VERSION_NUMBER v0.6.99
+#endif
+
+#ifndef LIBCROCO_VERSION
+#define LIBCROCO_VERSION "0.6.99-b9e4b47"
+#endif
+
+/*#ifndef G_DISABLE_CHECKS
+#if @G_DISABLE_CHECKS@
+#define G_DISABLE_CHECKS @G_DISABLE_CHECKS@
+#endif
+#endif*/
+
+#ifndef CROCO_HAVE_LIBXML2
+#define CROCO_HAVE_LIBXML2 (1)
+#endif
