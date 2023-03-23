116f8da7cd668826acf1a7f64269ac2155f6f164
Jamaika1 <lukaszcz18@wp.pl>
2023-03-23 16:42:46 +0100

    Added uavs3d uavs3/uavs3d@1fd0491

---
+++ libavcodec/libuavs3d/version.h
@@ -0,0 +1,17 @@
+// ===========================================================================
+// version.h
+// - collection of version numbers
+// ===========================================================================
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#define VER_MAJOR  1                // major version number
+#define VER_MINOR  2                // minor version number
+#define VER_BUILD  0                // build number
+
+#define VERSION_TYPE "release"
+#define VERSION_STR  "1.2.0"
+#define VERSION_SHA1 "0133ee4"
+
+#endif // __VERSION_H__
