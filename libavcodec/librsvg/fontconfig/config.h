b963d8d4291f9ce3684e970cd1cf7c8bf6a27ae1
Jamaika1 <lukaszcz18@wp.pl>
2023-04-12 17:21:37 +0200

    Added fontconfig freedesktop/fontconfig@ec3e747

---
+++ libavcodec/librsvg/fontconfig/config.h
@@ -0,0 +1,31 @@
+#define FLEXIBLE_ARRAY_MEMBER 1
+#define SIZEOF_VOID_P 8
+#define ALIGNOF_DOUBLE 8
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_FCNTL_H 1
+#define HAVE_MKSTEMP 1
+#define HAVE_RAND 1
+#define ENABLE_LIBXML2 1
+#define FC_GPERF_SIZE_T unsigned int
+#define FC_DEFAULT_FONTS "/usr/share/fonts"
+#define FC_FONTPATH "/etc/fonts"
+#define FC_CACHEDIR "/var/cache/fontconfig "
+#define FC_TEMPLATEDIR "/usr/share/fontconfig/conf.avail"
+#define CONFIGDIR "/etc/fonts/conf.d"
+
+#define FT_FREETYPE_H <freetype/freetype.h>
+#define FT_ADVANCES_H <freetype/ftadvanc.h>
+#define FT_TRUETYPE_TABLES_H <freetype/tttables.h>
+#define FT_SFNT_NAMES_H <freetype/ftsnames.h>
+#define FT_TRUETYPE_IDS_H <freetype/ttnameid.h>
+#define FT_TYPE1_TABLES_H <freetype/t1tables.h>
+#if HAVE_FT_GET_X11_FONT_FORMAT
+#define FT_XFREE86_H <freetype/ftfntfmt.h>
+#endif
+#if HAVE_FT_GET_BDF_PROPERTY
+#define FT_BDF_H <freetype/ftbdf.h>
+#define FT_MODULE_H <freetype/ftmodapi.h>
+#endif
+#define FT_MULTIPLE_MASTERS_H <freetype/ftmm.h>
+
