784817b844f130db8ea7445c49a664daf9aab8fd
Jamaika1 <lukaszcz18@wp.pl>
2023-08-10 08:37:20 +0200

    Added libavdevice FFmpeg/FFmpeg@7a2b587

--- libavdevice/extra/outdev_list.c
+++ libavdevice/extra/outdev_list.c
@@ -1,13 +1,13 @@
-static const AVOutputFormat * const outdev_list[] = {
+static const FFOutputFormat * const outdev_list[] = {
 //&ff_alsa_muxer,
 //&ff_audiotoolbox_muxer,
 //&ff_caca_muxer,
 //&ff_decklink_muxer,
 //&ff_fbdev_muxer,
-//&ff_opengl_muxer,
+&ff_opengl_muxer,
 //&ff_oss_muxer,
 //&ff_pulse_muxer,
-#ifdef FFPLUS
+#if CONFIG_SDL2
 &ff_sdl2_muxer,
 #endif
 //&ff_sndio_muxer,
