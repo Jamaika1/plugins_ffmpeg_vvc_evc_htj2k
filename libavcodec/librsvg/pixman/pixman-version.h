699f32087dc4ca863b97d5a82a74a752a791bc29
Jamaika1 <lukaszcz18@wp.pl>
2023-08-12 11:23:38 +0200

    Added pixman pixman/pixman@593a970 (#149)

--- libavcodec/librsvg/pixman/pixman-version.h
+++ libavcodec/librsvg/pixman/pixman-version.h
@@ -35,7 +35,7 @@
 #define PIXMAN_VERSION_MINOR 42
 #define PIXMAN_VERSION_MICRO 3
 
-#define PIXMAN_VERSION_STRING "0.42.3"
+#define PIXMAN_VERSION_STRING "0.42.3-593a970"
 
 #define PIXMAN_VERSION_ENCODE(major, minor, micro) (	\
 	  ((major) * 10000)				\
