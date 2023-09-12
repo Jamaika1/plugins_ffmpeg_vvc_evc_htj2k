ab77887bcdb63e6b524dad0fdf8983b69b375145
Jamaika1 <lukaszcz18@wp.pl>
2023-09-12 20:57:52 +0200

    Added FFmpeg FFmpeg/FFmpeg@a18ccbf

--- include/libavcodec/vvcdsp.h
+++ include/libavcodec/vvcdsp.h
@@ -58,19 +58,18 @@ typedef struct VVCInterDSPContext {
         const uint8_t *src, ptrdiff_t src_stride, int height, int denom, int wx, int ox,
         intptr_t mx, intptr_t my, int width, int hf_idx, int vf_idx);
 
-    void (*put_bi[2 /* luma, chroma */][2 /* int, frac */][2 /* int, frac */])(
-        uint8_t *dst, ptrdiff_t dst_stride, const uint8_t *src, ptrdiff_t src_stride, const int16_t *src2,
-        int height, intptr_t mx, intptr_t my, int width, int hf_idx, int vf_idx);
-    void (*put_bi_w[2 /* luma, chroma */][2 /* int, frac */][2 /* int, frac */])(
-        uint8_t *dst, ptrdiff_t dst_stride, const uint8_t *src, ptrdiff_t src_stride, const int16_t *src2,
-        int height, int denom, int wx0, int wx1, int ox0, int ox1,
-        intptr_t mx, intptr_t my, int width, int hf_idx, int vf_idx);
+    void (*avg)(uint8_t *dst, ptrdiff_t dst_stride,
+        const int16_t *src0, const int16_t *src1, int width, int height);
+
+    void (*w_avg)(uint8_t *_dst, const ptrdiff_t _dst_stride,
+        const int16_t *src0, const int16_t *src1, int width, int height,
+        int denom, int w0, int w1, int o0, int o1);
 
     void (*put_ciip)(uint8_t *dst, ptrdiff_t dst_stride, int width, int height,
         const uint8_t *inter, ptrdiff_t inter_stride, int inter_weight);
 
     void (*put_gpm)(uint8_t *dst, ptrdiff_t dst_stride, int width, int height,
-        const int16_t *tmp, const int16_t *tmp1, const ptrdiff_t tmp_stride,
+        const int16_t *src0, const int16_t *src1,
         const uint8_t *weights, int step_x, int step_y);
 
     void (*fetch_samples)(int16_t *dst, const uint8_t *src, ptrdiff_t src_stride, int x_frac, int y_frac);
@@ -85,11 +84,6 @@ typedef struct VVCInterDSPContext {
         const int16_t *diff_mv_x, const int16_t *diff_mv_y);
     void (*apply_prof_uni_w)(uint8_t *dst, const ptrdiff_t dst_stride, const int16_t *src,
         const int16_t *diff_mv_x, const int16_t *diff_mv_y, int denom, int wx, int ox);
-
-    void (*apply_prof_bi)(uint8_t *dst, ptrdiff_t dst_stride, const int16_t *src0, const int16_t *src1,
-        const int16_t *diff_mv_x, const int16_t *diff_mv_y);
-    void (*apply_prof_bi_w)(uint8_t *dst, ptrdiff_t dst_stride, const int16_t *src0, const int16_t *src1,
-        const int16_t *diff_mv_x, const int16_t *diff_mv_y, int denom, int w0, int w1, int o0, int o1);
 
     void (*apply_bdof)(uint8_t *dst, ptrdiff_t dst_stride, int16_t *src0, int16_t *src1, int block_w, int block_h);
 
@@ -132,10 +126,10 @@ typedef struct VVCLMCSDSPContext {
 typedef struct VVCLFDSPContext {
     int (*ladf_level[2 /* h, v */])(const uint8_t *pix, ptrdiff_t stride);
 
-    void (*filter_luma[2 /* h, v */])(uint8_t *pix, ptrdiff_t stride, int beta, int32_t tc,
-        uint8_t no_p, uint8_t no_q, uint8_t max_len_p, uint8_t max_len_q, int hor_ctu_edge);
-    void (*filter_chroma[2 /* h, v */])(uint8_t *pix, ptrdiff_t stride, int beta, int32_t tc,
-        uint8_t no_p, uint8_t no_q, int shift, int max_len_p, int max_len_q);
+    void (*filter_luma[2 /* h, v */])(uint8_t *pix, ptrdiff_t stride, const int32_t *beta, const int32_t *tc,
+        const uint8_t *no_p, const uint8_t *no_q, const uint8_t *max_len_p, const uint8_t *max_len_q, int hor_ctu_edge);
+    void (*filter_chroma[2 /* h, v */])(uint8_t *pix, ptrdiff_t stride, const int32_t *beta, const int32_t *tc,
+        const uint8_t *no_p, const uint8_t *no_q, const uint8_t *max_len_p, const uint8_t *max_len_q, int shift);
 } VVCLFDSPContext;
 
 struct SAOParams;
@@ -174,10 +168,7 @@ typedef struct VVCDSPContext {
 
 void ff_vvc_dsp_init(VVCDSPContext *hpc, int bit_depth);
 
-extern const int8_t ff_vvc_chroma_filters[3][32][4];
-extern const int8_t ff_vvc_luma_filters[3][16][8];
-extern const int8_t ff_vvc_dmvr_filters[16][2];
-
 void ff_vvc_dsp_init_x86(VVCDSPContext *c, const int bit_depth);
+void ff_vvc_dsp_init_aarch64(VVCDSPContext *c, const int bit_depth);
 
 #endif /* AVCODEC_VVCDSP_H */
