ef5d45d91d271916a2ea8431be30700f934e4e2a
Jamaika1 <lukaszcz18@wp.pl>
2023-12-04 16:08:07 +0100

    Fix config fontconfig

--- libavcodec/librsvg/fontconfig/config.h
+++ libavcodec/librsvg/fontconfig/config.h
@@ -3,7 +3,8 @@
 #define ALIGNOF_DOUBLE 8
 #define HAVE_SYS_TYPES_H 1
 #define HAVE_SYS_STAT_H 1
-#define HAVE_DIRENT_H 1
+//#define HAVE_SYS_PARAM_H 0
+//#define HAVE_DIRENT_H 1
 #define HAVE_FCNTL_H 1
 #define HAVE_MKSTEMP 1
 #define HAVE_RAND 1
--- libavcodec/librsvg/fontconfig/fontconfig.h
+++ libavcodec/librsvg/fontconfig/fontconfig.h
@@ -25,7 +25,7 @@
 #ifndef _FONTCONFIG_H_
 #define _FONTCONFIG_H_
 
-#ifdef HAVE_CONFIG_H
+#if HAVE_CONFIG_H
 #include "config.h"
 #endif
 
