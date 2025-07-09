3da243e0408f270bc8f08316b630a8218b139027
Jamaika1 <lukaszcz18@wp.pl>
2025-07-09 15:26:12 +0200

    Update libopenh264 20250709 #1 x100

---
+++ libavcodec/libopenh264/decoder/version.h
@@ -0,0 +1,10 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#ifdef GENERATED_VERSION_HEADER
+#include "version_gen.h"
+#else
+#define VERSION_NUMBER "OpenH264 version: 2.6.0.2508-0c9a557"
+#endif
+
+#endif // VERSION_H
