fb74e2ba3bf8fe14869eb348caab7e6b091c4ed7
Jamaika1 <lukaszcz18@wp.pl>
2025-07-09 22:06:13 +0200

    Update librsvg 20250709 #1 x100

---
+++ libavcodec/librsvg/librsvg-features.c
@@ -0,0 +1,8 @@
+#include "config.h"
+#include "rsvg.h"
+
+/* General initialization hooks */
+const guint librsvg_major_version = LIBRSVG_MAJOR_VERSION;
+const guint librsvg_minor_version = LIBRSVG_MINOR_VERSION;
+const guint librsvg_micro_version = LIBRSVG_MICRO_VERSION;
+const char librsvg_version[] = LIBRSVG_VERSION;
