3e9c2c33eac7a69617deeba744be02f10ddcd801
Jamaika1 <lukaszcz18@wp.pl>
2025-07-09 18:39:11 +0200

    Update libcroco 20250709 #1 x100

---
+++ libavcodec/librsvg/libcroco/cr-libxml-node-iface.h
@@ -0,0 +1,15 @@
+#ifndef __CR_LIBXML_NODE_IFACE_H__
+#define __CR_LIBXML_NODE_IFACE_H__
+
+#include "../glib/config_win32.h"
+#include "../glib/glib.h"
+#include "cr-node-iface.h"
+
+G_BEGIN_DECLS
+
+extern CRNodeIface const cr_libxml_node_iface;
+
+G_END_DECLS
+
+
+#endif/*__CR_LIBXML_NODE_IFACE_H__*/
