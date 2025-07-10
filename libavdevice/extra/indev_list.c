9b207a0a6b79c24e4b63ba4bda85259178b7e874
Jamaika1 <lukaszcz18@wp.pl>
2025-07-10 12:43:45 +0200

    Update libavdevice 20250709 #2 x100

---
+++ libavdevice/extra/indev_list.c
@@ -0,0 +1,24 @@
+static const FFInputFormat * const indev_list[] = {
+//&ff_alsa_demuxer,
+//&ff_android_camera_demuxer,
+//&ff_avfoundation_demuxer,
+//&ff_decklink_demuxer,
+&ff_dshow_demuxer,
+//&ff_fbdev_demuxer,
+&ff_gdigrab_demuxer,
+//&ff_iec61883_demuxer,
+//&ff_jack_demuxer,
+//&ff_kmsgrab_demuxer,
+&ff_lavfi_demuxer,
+//&ff_openal_demuxer,
+//&ff_oss_demuxer,
+//&ff_pulse_demuxer,
+//&ff_sndio_demuxer,
+//&ff_v4l2_demuxer,
+&ff_vfwcap_demuxer,
+//&ff_xcbgrab_demuxer,
+
+/* external libraries */
+//&ff_libcdio_demuxer,
+//&ff_libdc1394_demuxer,
+    NULL };
---
+++ libavdevice/extra/outdev_list.c
@@ -0,0 +1,12 @@
+static const FFOutputFormat * const outdev_list[] = {
+//&ff_alsa_muxer,
+//&ff_audiotoolbox_muxer,
+//&ff_caca_muxer,
+//&ff_decklink_muxer,
+//&ff_fbdev_muxer,
+//&ff_oss_muxer,
+//&ff_pulse_muxer,
+//&ff_sndio_muxer,
+//&ff_v4l2_muxer,
+//&ff_xv_muxer,
+    NULL };
